f27901a1f2140c3ef83cdf9ef2b1c4467c1e96e5
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-09-10 14:05:00 +0200

    Manage Members

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,11 +1,7 @@
 import { create } from "zustand";
 import { Server } from "@prisma/client";
 
-export type ModalType =
-  | "createServer"
-  | "invite"
-  | "editServer"
-  | "editChannel";
+export type ModalType = "createServer" | "invite" | "editServer" | "members";
 
 interface ModalData {
   server?: Server;
